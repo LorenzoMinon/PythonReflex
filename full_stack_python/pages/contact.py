aea9c524353778bce432ee7d7dfd83cb75b88755
Lorenzo Miñón <lorenzominon01@gmail.com>
2024-10-04 11:03:54 -0300

    responsive form

--- full_stack_python/pages/contact.py
+++ full_stack_python/pages/contact.py
@@ -19,19 +19,31 @@ class ContactState(rx.State):
 def contact_page() -> rx.Component:
     my_form = rx.form(
             rx.vstack(
-                rx.input(
-                    placeholder="First Name",
-                    name="first_name",
+                rx.hstack(
+                    rx.input(
+                        placeholder="First Name",
+                        name="first_name",
+                        width="100%",
+                    ),
+                    rx.input(
+                        placeholder="Last Name",
+                        name="last_name",
+                        width="100%",
+                    ),
+                    width='100%'
                 ),
                 rx.input(
-                    placeholder="Last Name",
+                    placeholder="Your email",
                     name="last_name",
+                    width="100%",
                 ),
                 rx.text_area(
                     name="message",
-                    placeholder="your msg"
+                    placeholder="your msg",
+                    width="100%",
                 ),
                 rx.button("Submit", type="submit"),
+                width="100%",
             ),
             on_submit=ContactState.handle_submit,
             reset_on_submit=True,
@@ -39,7 +51,20 @@ def contact_page() -> rx.Component:
 
     my_child=rx.vstack(
             rx.heading("Contact", size="9"),
-            my_form,
+            rx.desktop_only(
+                rx.box(
+                    my_form,
+                    id="my-form-box",
+                    width="25vw",
+                )
+            ),
+            rx.mobile_and_tablet(
+                rx.box(
+                    my_form,
+                    id="my-form-box",
+                    width="55vw",
+                )
+            ),
             rx.text(
                 "Sth cool about us contact! ",
             ),
